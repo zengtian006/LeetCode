fdcff6fc831f6404e3b6cd58d850481de734f39a
Tian Zeng <Tian.Zeng@aexp.com>
2020-04-16 18:09:57 +0800

    Pow(x, n)

--- Math/Leetcode 50. Pow(x, n).py
+++ Math/Leetcode 50. Pow(x, n).py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution1:
     def myPow(self, x: float, n: int) -> float:
         if n<0:
             x = 1/x
@@ -14,3 +14,19 @@ class Solution:
     
    # 2^8 = 4^4
    # 2^9 = 2*4^4 
+
+   #Recursive
+   class Solution2:
+    def myPow(self, x: float, n: int) -> float:
+        if n<0:
+            return self.helper(1/x,-n)
+        return self.helper(x,n)
+    
+    def helper(self, x,n):
+        if n == 0:
+            return 1
+        half = self.helper(x, n//2)
+        if n % 2 == 1:
+            return x * half * half
+        else:
+            return half * half
